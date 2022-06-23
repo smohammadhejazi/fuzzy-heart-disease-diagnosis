2f7c226fd6e549a4c30c2a168858fa5d615811f4
Mohammad Hejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-06-23 14:01:08 +0430

    write code to fuzzify all input values

--- final_result.py
+++ final_result.py
@@ -1,4 +1,4 @@
-from fuzzification import Fuzzifier
+from fuzzification import HeartDiseaseFuzzifier
 
 class ProvideResult(object):
     def __new__(cls):
@@ -8,4 +8,9 @@ class ProvideResult(object):
 
     @staticmethod
     def get_final_result(input_dict: dict) -> str:
-        pass
+        main_fuzzifier = HeartDiseaseFuzzifier()
+        fuzzy_values = {}
+
+        # get fuzzy version of all values
+        for param, value in input_dict.items():
+            fuzzy_values[param] = main_fuzzifier.fuzzifiers[param].fuzzify(value)

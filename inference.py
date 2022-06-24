8f63f1b0b4e3c0d1be416257356b17307f9e5b5f
smohammadhejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-06-24 16:53:08 +0430

    complete Rule and Inference class

--- inference.py
+++ inference.py
@@ -1,23 +1,33 @@
 class Rule:
-    def __init__(self, condition, result):
-        self.imply_func = None
+    def __init__(self, condition, action):
+        self._imply_func = None
         self.condition = condition
-        self.result = result
+        self.action = action
         self.initialize()
 
     def define_imply_func(self, condition_parts, min_max):
         def func(input_params):
             final = None
+            # if there is only one condition
             if min_max is None:
-                return {self.result.split(" IS ")[1]: final}
+                param, p_type = condition_parts.replace(" ", "").replace("\t", "").split("IS")
+                if param in input_params:
+                    final = input_params[param][p_type]
+            # if there are multiple conditions
             else:
                 for condition_part in condition_parts:
-                    param, p_type = condition_part.split(" IS ")
-                    if final is None:
-                        final = input_params[param][p_type]
-                    else:
-                        final = min_max(final, input_params[param][p_type])
-                return {self.result.split(" IS ")[1]: final}
+                    param, p_type = condition_part.replace(" ", "").replace("\t", "").split("IS")
+                    if param in input_params:
+                        if final is None:
+                            final = input_params[param][p_type]
+                        else:
+                            final = min_max(final, input_params[param][p_type])
+
+            # if nothing fired, final is zero
+            if final is None:
+                final = 0
+
+            return {self.action.replace(" ", "").replace("\t", "").split("IS")[1]: final}
 
         return func
 
@@ -31,7 +41,7 @@ class Rule:
             condition_parts = [x.strip("() ") for x in condition_parts]
             self.imply_func = self.define_imply_func(condition_parts, max)
         else:
-            self.imply_func = self.define_imply_func(self.condition, None)
+            self.imply_func = self.define_imply_func(self.condition.strip("() "), None)
 
     def imply(self, inputs):
         if self.imply_func is None:
@@ -43,22 +53,34 @@ class Rule:
 class Inference:
     def __init__(self):
         self.rules = []
+        self.initialize_rules()
 
     def initialize_rules(self):
         with open("rules.fcl") as rules:
             for line in rules:
-                main = line.strip(";\n").split(":")[1]
-                if_then = main.split("THEN")
-                condition = if_then[0][4:].strip()
-                result = if_then[1].strip()
-                rule = Rule(condition, result)
-                self.rules.append(rule)
+                if line.strip(";\n") != "":
+                    if_then_string = line.strip(";\n").split(":")[1]
+                    if_then_parts = if_then_string.split("THEN")
+                    condition = if_then_parts[0][4:].strip()
+                    action = if_then_parts[1].strip()
+                    rule = Rule(condition, action)
+                    self.rules.append(rule)
+
+    def imply_all(self, input_params):
+        final = {}
+        for rule in self.rules:
+            result = rule.imply(input_params)
+            for key, value in result.items():
+                if key not in final:
+                    final[key] = value
+                else:
+                    final[key] = max(final[key], value)
+        return final
 
 
 # Test
 if __name__ == "__main__":
     inference = Inference()
-    inference.initialize_rules()
     inputs = {
         "age": {
             "young": 0.5,
@@ -73,6 +95,5 @@ if __name__ == "__main__":
             "asymptomatic": 0
         },
     }
-
-    print(inference.rules[0].imply(inputs))
-
+    # print(inference.rules[11].imply(inputs))
+    print(inference.imply_all(inputs))

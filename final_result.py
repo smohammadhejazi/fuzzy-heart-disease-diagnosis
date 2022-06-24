c5a8c93d4f53c16e21b65008d09738f55aca7bb3
smohammadhejazi <78293534+smohammadhejazi@users.noreply.github.com>
2022-06-24 22:52:10 +0430

    complete the pipeline

--- final_result.py
+++ final_result.py
@@ -1,5 +1,6 @@
 from fuzzification import HeartDiseaseFuzzifier
 from inference import Inference
+from defuzzification import HeartDiseaseDefuzzifier
 
 
 class ProvideResult(object):
@@ -10,40 +11,25 @@ class ProvideResult(object):
 
     @staticmethod
     def get_final_result(input_dict: dict) -> str:
-        input_dict = {'chest_pain': '1', 'cholestrol': '0', 'ecg': '0', 'exercise': '0', 'thallium_scan': '3',
-                      'age': '0', 'blood_pressure': '0', 'blood_sugar': '0', 'heart_rate': '0', 'old_peak': '0',
-                      'sex': '0'
-                      }
-
         heart_disease_fuzzifier = HeartDiseaseFuzzifier()
         inference = Inference()
+        heart_disease_defuzzifier = HeartDiseaseDefuzzifier(500)
 
         # tidy input dict
         input_dict["cholesterol"] = input_dict.pop("cholestrol")
         input_dict["thallium"] = input_dict.pop("thallium_scan")
         input_dict["maximum_heart_rate"] = input_dict.pop("heart_rate")
 
-        heart_disease_fuzzifier = HeartDiseaseFuzzifier()
-        fuzzy_values = {}
+        fuzzy_values = heart_disease_fuzzifier.fuzzify_all(input_dict)
+        print(fuzzy_values)
+        fuzzy_outputs = inference.imply_all(fuzzy_values)
+        print(fuzzy_outputs)
+        output = heart_disease_defuzzifier.deffuzzify(fuzzy_outputs)
 
-        # get fuzzy version of all values
-        for param, value in input_dict.items():
-            fuzzy_values[param] = heart_disease_fuzzifier.fuzzifiers[param].fuzzify(int(value))
-
-        blood_sugar = {"true": 0, "false": 0}
-        if fuzzy_values.pop("blood_sugar")["very_high"] > 120:
-            blood_sugar["true"] = 1
-            blood_sugar["true"] = 0
-        else:
-            blood_sugar["true"] = 0
-            blood_sugar["true"] = 1
-        fuzzy_values["blood_sugar"] = blood_sugar
+        return output
 
 
-
-        print(inference.imply_all(fuzzy_values))
-
-
+# Test
 if __name__ == "__main__":
     provideResult = ProvideResult()
     input_dict = {'chest_pain': '1', 'cholestrol': '0', 'ecg': '0', 'exercise': '0', 'thallium_scan': '3', 'age': '0',
